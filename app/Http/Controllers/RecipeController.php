c402f544ab1a33790ef96b36c61a49212c02384f
Rene Dyhr <rd@cleanmanager.dk>
2023-07-31 14:30:29 +0000

    test: made the recipe controller and test work better and more clean

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -3,58 +3,65 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use App\Models\Ingredient;
 use App\Models\Recipe;
-use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RecipeController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'name' => 'required|max:255',
             'description' => 'required',
             'public' => 'required|boolean',
             'categories' => 'required|array',
-            'ingredients' => 'required',
-            'tags' => 'required',
+            'ingredients' => 'required|array',
+            'tags' => 'required|array',
         ]);
 
+        $recipe = $this->createRecipe($request);
+
+        $recipe->load('categories', 'ingredients', 'tags');
+
+        return response()->json($recipe, 201);
+    }
+
+    private function createRecipe(Request $request): Recipe
+    {
         $fields = [
-            ...$request->all(),
-            "user_id" => Auth::id(),
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'note' => $request->input('note'),
+            'public' => $request->input('public'),
+            'user_id' => Auth::id(),
         ];
 
-        $notFound = 0;
-        foreach($request->get('categories') as $category) {
-            try {
-                Category::query()->where('id', $category)->where('user_id', Auth::id())->firstOrFail();
-            } catch(\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-                $notFound = 1;
-            }
-        }
-        if ($notFound) {
-            return response()->json(["error" => "Category not found or owned"], 404);
-        }
+        $categories = $request->input('categories');
+        $this->validateCategories($categories);
 
         $recipe = Recipe::create($fields);
-        $recipe->categories()->attach($request->get('categories'));
-        foreach ($request->get('ingredients') as $ingredient) {
-            $recipe->ingredients()->create(['name' => $ingredient]);
-        }
-        foreach ($request->get('tags') as $tag) {
-            $recipe->tags()->create(['name' => $tag]);
-        }
 
-        $recipe->refresh();
-        $loadout = [
-            ...$recipe->toArray(),
-            'ingredients' => $recipe->ingredients,
-            'tags' => $recipe->tags,
-            'categories' => $recipe->categories,
-        ];
-        
-        return response()->json($loadout, 201);
+        $recipe->categories()->attach($categories);
+
+        $ingredients = $request->input('ingredients');
+        $recipe->ingredients()->createMany(array_map(fn($name) => ['name' => $name], $ingredients));
+
+        $tags = $request->input('tags');
+        $recipe->tags()->createMany(array_map(fn($name) => ['name' => $name], $tags));
+
+        return $recipe;
+    }
+
+    private function validateCategories(array $categories): void
+    {
+        $user_id = Auth::id();
+        $notFound = count($categories) - Category::whereIn('id', $categories)
+            ->where('user_id', $user_id)
+            ->count();
+
+        if ($notFound) {
+            abort(404, "Category not found or owned");
+        }
     }
 }

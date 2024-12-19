2e8908e1b650499d65d509444a10566c882851d1
Rene Dyhr <rd@cleanmanager.dk>
2024-12-19 07:40:15 +0000

    feat: add header component and refactor layout to include it; update scripts and styles in app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,12 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/font-awesome.css">
+    <link rel="stylesheet" href="/css/jquery-ui.css">
+    <link rel="stylesheet" href="/css/datetimepicker.min.css">
+    <script src="/js/jquery.js"></script>
+    <script src="/js/masonry.js"></script>
+    <script src="/js/jquery-ui.js"></script>
+    <script src="/js/datetimepicker.js"></script>
+    <script src="/js/bootstrap-tagsinput.js"></script>
+    <script src="/js/jquery.multi-select.js"></script>
+    <script src="/js/jquery.select.js"></script>
+    <script src="/js/jquery.mobile-1.4.5.min.js"></script>
     @vite('resources/scss/app.scss')
-    @vite('resources/css/datetimepicker.min.css')
-    @vite('resources/css/font-awesome.css')
-    @vite('resources/css/jquery-ui.css')
-    @vite('resources/js/jquery.js')
-    @vite('resources/js/masonry.js')
     @vite('resources/js/app.js')
     <title>{{ $title ?? 'Page Title' }}</title>
 </head>
--- resources/views/livewire/recipes/index.blade.php
+++ resources/views/livewire/recipes/index.blade.php
@@ -1,87 +1,7 @@
 <div>
 @include('components.layouts.sidenav')
 <div id="main">
-    <div class="header">
-        <div class="menu">
-            <a href="javascript:void(0)" id="closenav" style="display:none;" onclick="closeNav()">
-            <i class="fa fa-bars"></i>
-            </a>
-            <a href="javascript:void(0)" id="opennav" onclick="openNav()">
-            <i class="fa fa-bars"></i>
-            </a>
-        </div>
-        <a href="/" class="logo">
-        <img src="/images/logo.png">
-        </a>
-        <div class="categories">
-            <div class="center">
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Temporary">
-                    <a href="/category/1-temporary">
-                    <i class="icon icon-milk"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Bakery">
-                    <a href="/category/2-bakery">
-                    <i class="icon icon-bread"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Cakes/Cookies">
-                    <a href="/category/3-cakes-cookies">
-                    <i class="icon icon-cookie"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Dessert">
-                    <a href="/category/4-dessert">
-                    <i class="icon icon-cake"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Vegan">
-                    <a href="/category/5-vegan">
-                    <i class="icon icon-leaf"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Vegetarian">
-                    <a href="/category/6-vegetarian">
-                    <i class="icon icon-leaf"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Meat">
-                    <a href="/category/7-meat">
-                    <i class="icon icon-steak"></i>
-                    </a>
-                </div>
-                <div class="list-group-item" data-toggle="tooltip" data-placement="top"
-                    title="Toppings">
-                    <a href="/category/23-toppings">
-                    <i class="icon icon-bread"></i>
-                    </a>
-                </div>
-                <div class="btn-group" style="margin-left:30px;">
-                    <i class="icon-loop dropdown-toggle" data-toggle="dropdown"></i>
-                    <div class="dropdown-menu" style="right:34px;left:inherit;top:-10px;padding:5px;">
-                        <form method="get" action="/recipe/search">
-                            <div class="input-group" style="margin:0;">
-                                <input class="form-control" name="q" value="" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="submit">
-                                <i class="fa fa-search"></i>
-                                </button>
-                                </span>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="clear"></div>
-            </div>
-        </div>
-    </div>
+    @include('components.layouts.header')
     <div class="content homepage">
         <div class="col-12 recipe-list">
             <div class="list">
@@ -93,7 +13,11 @@
                         <div class="recipe-list">
                             <ul>
                                 @foreach ($recipe->ingredients as $ingredient)
-                                    <li>{{$ingredient->name}}</li>
+                                    @if (str_starts_with($ingredient->name, '#'))
+                                        <li><b>{{substr($ingredient->name, 1)}}</b></li>
+                                    @else
+                                        <li>{{$ingredient->name}}</li>
+                                    @endif
                                 @endforeach
                             </ul>
                         </div>
@@ -101,8 +25,8 @@
                             <div class="tags">
                                 <label>Tags:</label>
                                 <span>
-                                    @foreach ($recipe->tags as $tag)
-                                        <a href="/tag/{{$tag->name}}">{{$tag->name}}</a>
+                                    @foreach($recipe->tags as $index => $tag)
+                                        <a href="/tag/{{$tag->name}}">{{$tag->name}}</a>@if($index < count($recipe->tags) - 1), @endif
                                     @endforeach
                                 </span>
                                 <div class="clear"></div>
@@ -112,8 +36,8 @@
                             <div class="tags">
                                 <label>Categories:</label>
                                 <span>
-                                    @foreach ($recipe->categories as $category)
-                                        <a href="/category/{{$category->id}}-{{$category->name}}">{{$category->name}}</a>
+                                    @foreach ($recipe->categories as $index => $category)
+                                        <a href="/category/{{$category->id}}-{{$category->name}}">{{$category->name}}</a>@if($index < count($recipe->categories) - 1), @endif
                                     @endforeach
                                 </span>
                                 <div class="clear"></div>

0e2d84170c46f625076117fe0adb4c354a397bf6
Rene Dyhr <rd@cleanmanager.dk>
2024-12-19 07:41:23 +0000

    feat: add Icon model and relationship to Category; update categories migration to use icon_id

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Category extends Model
 {
@@ -29,5 +30,21 @@ class Category extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get icon connected
+     */
+    public function icon()
+    {
+        return $this->belongsTo(Icon::class);
+    }
+
+    /**
+     * Scope a query to only include categories of the authenticated user.
+     */
+    public function scopeForAuthUser($query)
+    {
+        return $query->where('user_id', Auth::id());
     }
 }
--- database/migrations/2024_12_17_190824_icons.php
+++ database/migrations/2024_12_17_190824_icons.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('icons', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('icon_id')->constrained()->onDelete('cascade');
-            $table->string('slug');
             $table->string('name');
+            $table->string('class');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('icons');
     }
 };

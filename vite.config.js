be29298d2d71fa37df9c1ed7fe192a8502c9ed48
Rene Dyhr <rd@cleanmanager.dk>
2024-12-31 11:33:07 +0000

    feat: add process.env definition to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    define: {
+        'process.env': process.env,
+    },
     plugins: [
         laravel({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
